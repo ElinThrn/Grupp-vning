515182efd23c0f2ca9b0bbdc53a640611a5dddc5
Viktor Karlström <46776740+ViktorKarlstrom@users.noreply.github.com>
2020-10-08 11:23:29 +0200

    Skapat file manager Skapat Write metod Lagt till filepath i App.config

---
+++ Gruppövning/Classes/FileManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Gruppövning.Classes
+{
+    public class FileManager
+    {
+        public static void Write(Recipe recipe)
+        {
+            string recipeInput = string.Join(";", recipe.Id, recipe.Title, recipe.Description, recipe.Category) + "%";
+
+            foreach (var ingredient in recipe.IngredientsValue)
+            {
+                string.Join(";", ingredient.Value, ingredient.Key);
+                recipeInput += string.Join(";", ingredient.Value, ingredient.Key);
+            }
+
+            using (StreamWriter writer  = new StreamWriter(ConfigurationManager.AppSettings["recipeFile"], true))
+            {
+                writer.WriteLine(recipeInput);
+            }
+        }
+
+        public static void Update(Recipe recipe)
+        {
+            
+        }
+        public static void Read()
+        {
+
+        }
+        public static void Delete()
+        {
+
+        }
+    }
+}
