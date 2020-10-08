074be9d2d64b4098764a16d406b67ee2bcb779d2
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2020-10-08 10:36:14 +0200

    Added Guid

--- Gruppövning/Classes/Recipe.cs
+++ Gruppövning/Classes/Recipe.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gruppövning.Classes
 {
     public class Recipe
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public Dictionary<string, string> AmountOfIngredients { get; set; }
+        public Dictionary<string, string> IngredientsValue { get; set; }
 
         public Recipe(string title, string description, string category, Dictionary<string, string> ingredients)
         {
-            this.Title = Title;
+            Id = Guid.NewGuid();
+            this.Title = title;
             this.Description = description;
+            this.Category = category;
+            this.IngredientsValue = ingredients;
         }
+
+        public void Save()
+        {
+            //TODO: change to FileManager MethodCall
+            //SaveFile(this.Id, this.Title, this.Description, this.Category, this.IngredientsValue);
+        }
+
+        public void SaveEdited()
+        {
+            //TODO: change to FileManager MethodCall
+            //SaveEditedFile(this.Id, this.Title, this.Description, this.Category, this.IngredientsValue);
+        }
+
 
     }
 }
