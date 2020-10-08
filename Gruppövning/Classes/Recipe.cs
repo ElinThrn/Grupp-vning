d40684ac35879f155321540e41088fb53181a901
Viktor Karlström <46776740+ViktorKarlstrom@users.noreply.github.com>
2020-10-08 09:41:53 +0200

    Lagt till receptklass och dess props

---
+++ Gruppövning/Classes/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppövning.Classes
+{
+    public class Recipe
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public Dictionary<string, string> AmountOfIngredients { get; set; }
+
+    }
+}
